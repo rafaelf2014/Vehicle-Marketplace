6b3c03a72f8a6c58317ed5e864e50cf17200a774
silvalukas <al81362@alunos.utad.pt>
2025-11-04 09:09:42 +0000

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CliCarProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
