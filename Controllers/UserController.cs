1f8c4a5b420cd8457954e3823461330cfb008504
silvalukas <al81362@alunos.utad.pt>
2025-11-04 10:10:33 +0000

    Criei 3 controllers: Admin User Car
    E as respetivas views, mas apenas para User e Car.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CliCarProject.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+    }
+}
