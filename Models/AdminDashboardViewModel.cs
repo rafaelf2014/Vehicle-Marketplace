618a83b745b7a0a46358cc265b7e6ce285af58d9
Rafael <Rafael@DESKTOP-095O1T9>
2025-12-27 17:17:20 +0000

    Base para painel de controlo

---
+++ Models/AdminDashboardViewModel.cs
@@ -0,0 +1,9 @@
+namespace CliCarProject.Models
+{
+    public class AdminDashboardViewModel
+    {
+        public int TotalUsers { get; set; }
+        public int TotalAnuncios { get; set; }
+        public int TotalVeiculos { get; set; }
+    }
+}
