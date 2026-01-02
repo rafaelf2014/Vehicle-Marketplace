75cf7e57120ba8ce585323e4d46e3d12d94954cc
Rafael <Rafael@DESKTOP-095O1T9>
2026-01-02 19:58:15 +0000

    Historico ações, criar conta e blocks

---
+++ Models/HistoricoAdminViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace CliCarProject.Models
+{
+    public class HistoricoAdminViewModel
+    {
+        public int IdHistorico { get; set; }
+
+        public string IdUtilizador { get; set; } = null!;
+
+        public string UserName { get; set; } = null!;
+
+        public string NomeAcao { get; set; } = null!;
+
+        public string TipoAcao { get; set; } = null!;
+
+        public int? IdAlvo { get; set; }
+
+        public string? TipoAlvo { get; set; }
+
+        public string? Razao { get; set; }
+
+        public DateTime? DataHora { get; set; }
+    }
+}
+
